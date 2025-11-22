8f8be8fcfaee88984beb11521049eaa188f6b5a3
Kavya <135150115+CaptainOverride@users.noreply.github.com>
2025-11-22 22:24:09 +0530

    fix: replace zustand with useState to avoid crash

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,19 @@
+import { useState } from 'react'
 import { Canvas } from '@react-three/fiber'
-import { EffectComposer, Bloom } from '@react-three/postprocessing'
 import { Ship } from './Ship'
-import { Background } from './Background'
-import { useStore } from './store'
 import './App.css'
 
 function App() {
-  const { gameStarted, startGame } = useStore((state) => ({
-    gameStarted: state.gameStarted,
-    startGame: state.startGame
-  }))
+  const [gameStarted, setGameStarted] = useState(false)
 
   return (
-    <div id="canvas-container" onClick={() => !gameStarted && startGame()}>
+    <div id="canvas-container" onClick={() => !gameStarted && setGameStarted(true)}>
       <Canvas camera={{ position: [0, 0, 5] }}>
         <color attach="background" args={['#050505']} />
         <ambientLight intensity={0.5} />
         <pointLight position={[10, 10, 10]} />
 
-        {/* <Background /> */}
         <Ship />
-
-        {/* <EffectComposer>
-          <Bloom luminanceThreshold={0} luminanceSmoothing={0.9} height={300} />
-        </EffectComposer> */}
       </Canvas>
 
       <div className={`ui-layer ${gameStarted ? 'game-active' : 'game-start'}`}>
