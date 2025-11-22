201249f7177ca430746004f0d738bd58635b63d5
Kavya <135150115+CaptainOverride@users.noreply.github.com>
2025-11-22 22:19:07 +0530

    fix: replace Vector3 with plain objects in store to prevent crash

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,35 @@
 import { Canvas } from '@react-three/fiber'
+import { EffectComposer, Bloom } from '@react-three/postprocessing'
 import { Ship } from './Ship'
+import { Background } from './Background'
+import { useStore } from './store'
 import './App.css'
 
 function App() {
+  const { gameStarted, startGame } = useStore((state) => ({
+    gameStarted: state.gameStarted,
+    startGame: state.startGame
+  }))
+
   return (
-    <div id="canvas-container">
+    <div id="canvas-container" onClick={() => !gameStarted && startGame()}>
       <Canvas camera={{ position: [0, 0, 5] }}>
         <color attach="background" args={['#050505']} />
         <ambientLight intensity={0.5} />
         <pointLight position={[10, 10, 10]} />
 
+        <Background />
         <Ship />
+
+        <EffectComposer>
+          <Bloom luminanceThreshold={0} luminanceSmoothing={0.9} height={300} />
+        </EffectComposer>
       </Canvas>
 
-      <div className="ui-layer game-start">
+      <div className={`ui-layer ${gameStarted ? 'game-active' : 'game-start'}`}>
         <div className="header">
           <h1>STARDRIFT</h1>
-          <p className="blink">PRESS WASD TO FLY</p>
+          {!gameStarted && <p className="blink">CLICK TO START</p>}
         </div>
       </div>
     </div>
--- src/store.js
+++ src/store.js
@@ -1,13 +1,17 @@
 import { create } from 'zustand'
-import { Vector3 } from 'three'
 
 export const useStore = create((set) => ({
     score: 0,
     gameStarted: false,
-    shipPosition: new Vector3(0, 0, 0),
-    explosionData: null, // { position: Vector3, id: number }
+    shipPosition: { x: 0, y: 0, z: 0 }, // Plain object instead of Vector3
+    explosionData: null, // { position: {x, y, z}, id: number }
     increaseScore: () => set((state) => ({ score: state.score + 1 })),
     startGame: () => set({ gameStarted: true }),
-    setShipPosition: (pos) => set({ shipPosition: pos }),
-    triggerExplosion: (position) => set({ explosionData: { position, id: Math.random() } }),
+    setShipPosition: (pos) => set({ shipPosition: { x: pos.x, y: pos.y, z: pos.z } }),
+    triggerExplosion: (position) => set({
+        explosionData: {
+            position: { x: position.x, y: position.y, z: position.z },
+            id: Math.random()
+        }
+    }),
 }))
