1f260a0584db6b49175b675073364011180fba74
Kavya <135150115+CaptainOverride@users.noreply.github.com>
2025-11-22 22:16:50 +0530

    chore: remove store from app completely

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,23 @@
 import { Canvas } from '@react-three/fiber'
-import { EffectComposer, Bloom } from '@react-three/postprocessing'
 import { Ship } from './Ship'
-import { Background } from './Background'
-import { Collectibles } from './Collectibles'
-import { ParticleSystem } from './ParticleSystem'
-import { useStore } from './store'
 import './App.css'
 
 function App() {
-  const { score, gameStarted, startGame } = useStore((state) => ({
-    score: state.score,
-    gameStarted: state.gameStarted,
-    startGame: state.startGame
-  }))
-
   return (
-    <div id="canvas-container" onClick={() => !gameStarted && startGame()}>
+    <div id="canvas-container">
       <Canvas camera={{ position: [0, 0, 5] }}>
         <color attach="background" args={['#050505']} />
         <ambientLight intensity={0.5} />
         <pointLight position={[10, 10, 10]} />
 
-        {/* <Background /> */}
         <Ship />
-        {/* {gameStarted && <Collectibles />} */}
-        {/* {gameStarted && <ParticleSystem />} */}
-
-        {/* <EffectComposer>
-          <Bloom luminanceThreshold={0} luminanceSmoothing={0.9} height={300} />
-        </EffectComposer> */}
       </Canvas>
 
-      <div className={`ui-layer ${gameStarted ? 'game-active' : 'game-start'}`}>
+      <div className="ui-layer game-start">
         <div className="header">
           <h1>STARDRIFT</h1>
-          {!gameStarted && <p className="blink">CLICK TO START</p>}
+          <p className="blink">PRESS WASD TO FLY</p>
         </div>
-
-        {gameStarted && <div className="score-board">SCORE: {score}</div>}
       </div>
     </div>
   )
