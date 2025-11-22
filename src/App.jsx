2e727edbacbb4beb092e5727a181769cd483b47d
Kavya <135150115+CaptainOverride@users.noreply.github.com>
2025-11-22 21:59:04 +0530

    chore: minimal debug scene to isolate rendering issue

--- src/App.jsx
+++ src/App.jsx
@@ -8,38 +8,15 @@ import { useStore } from './store'
 import './App.css'
 
 function App() {
-  const { score, gameStarted, startGame } = useStore((state) => ({
-    score: state.score,
-    gameStarted: state.gameStarted,
-    startGame: state.startGame
-  }))
-
   return (
-    <div id="canvas-container" onClick={() => !gameStarted && startGame()}>
-      <Canvas camera={{ position: [0, 0, 5] }}>
-        <color attach="background" args={['#050505']} />
-        <ambientLight intensity={0.5} />
-        <pointLight position={[10, 10, 10]} />
-
-        <Background />
-
-        <Ship />
-        {gameStarted && <Collectibles />}
-        <ParticleSystem />
-
-        {/* <EffectComposer>
-          <Bloom luminanceThreshold={0} luminanceSmoothing={0.9} height={300} />
-        </EffectComposer> */}
+    <div id="canvas-container" style={{ width: '100vw', height: '100vh', background: 'red' }}>
+      <Canvas>
+        <ambientLight />
+        <mesh>
+          <boxGeometry />
+          <meshBasicMaterial color="blue" />
+        </mesh>
       </Canvas>
-
-      <div className={`ui-layer ${gameStarted ? 'game-active' : 'game-start'}`}>
-        <div className="header">
-          <h1>STARDRIFT</h1>
-          {!gameStarted && <p className="blink">CLICK TO START</p>}
-        </div>
-
-        {gameStarted && <div className="score-board">SCORE: {score}</div>}
-      </div>
     </div>
   )
 }
